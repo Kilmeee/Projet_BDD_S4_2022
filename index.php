58383ad10e85bfad8c12e5c3444cb3f4c5a1c347
xaerose <alexandre.germonville-bellet8@etu.univ-lorraine.fr>
2022-03-03 11:32:20 +0100

    Q3

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 use gamepedia\db\Eloquent;
-use gamepedia\models\Jeu;
+use gamepedia\models\{Plateforme, Jeu};
 
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
@@ -11,4 +11,11 @@ print_r("Q1 : \n");
 $gamesMario = Jeu::where('name', 'LIKE', '%Mario%')->get();
 foreach ($gamesMario as $game) {
     print_r($game->name . "\n");
+}
+
+//Q3
+print_r("Q3 : \n");
+$installBasePlat = Plateform::where('install_base', '>=', '10000000')->get();
+foreach ($installBasePlat as $plat) {
+    print_r($plat->name . "\n");
 }
