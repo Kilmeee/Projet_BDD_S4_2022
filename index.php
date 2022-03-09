7233503f1ef35f37f4a8cbb2acc6dc94d9876f8b
xaerose <alexandre.germonville-bellet8@etu.univ-lorraine.fr>
2022-03-09 15:07:00 +0100

    update q5

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <?php
 
 use gamepedia\db\Eloquent;
-use gamepedia\models\{Compagnie, Jeu, Plateforme};
+use gamepedia\models\{Compagnie, Jeu, Plateforme, Rating};
 
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 Eloquent::start(__DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'conf' . DIRECTORY_SEPARATOR . 'conf.ini');
+
+
+
+
+
+
+
 
 //Q3
 $jeuxSony = Jeu::where('compagnie_id', '=', Compagnie::where('nom', 'like', '%Sony%')->first()->id)->get();
@@ -16,3 +23,7 @@ foreach($jeuxSony as $jeu) {
 $ratingsMario = Rating::has('jeux', function($j){
     $j->where('nom', 'like', '%Mario%');
 })->get();
+
+foreach($ratingsMario as $rating) {
+    echo $rating->nom . '<br>';
+}
