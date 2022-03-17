886a7c0a3056da910cbcdfa10bc2a224a248d140
xaerose <alexandre.germonville-bellet8@etu.univ-lorraine.fr>
2022-03-17 09:48:12 +0100

    q1,q2,q3,q4 partie 1 (speedtest)

--- README.md
+++ README.md
@@ -8,7 +8,14 @@
 
 # Partie 1 :
 
-### Q1 :
+###Q1 : 
+Temps d'éxecution de la requete qui liste l'ensemble des jeux: 135 000.86 millisecondes
+###Q2 : 
+Temps d'éxecution de la requete qui liste les jeux dont le nom contient 'Mario' : 1 091.39 millisecondes
+###Q3 : 
+Temps d'éxecution de la requete qui liste les personnages des jeux dont le nom débute par 'Mario' : 3 938.84 millisecondes
+###Q4 :
+Temps d'éxecution de la requete qui liste les jeux dont le nom débute par 'Mario' et dont le rating initial contient '3+' : 612.02 millisecondes
 
 
 ---
--- index.php
+++ index.php
@@ -1,6 +1,48 @@
 <?php
 
 use gamepedia\db\Eloquent;
+use gamepedia\models\Jeu;
 
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 Eloquent::start(__DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'conf' . DIRECTORY_SEPARATOR . 'conf.ini');
+
+//ini_set('memory_limit', '-1');
+//Partie 1
+
+//Q1
+print_r("Q1 : \n");
+$start1 = hrtime(true);
+$q1 = Jeu::all();
+$end1 = hrtime(true);
+$time1 = number_format((($end1 - $start1)/1e+6), 2, '.', ' ');
+print_r("Temps execution : " . $time1 . "\n");
+
+//Q2
+print_r("Q2 : \n");
+$start2 = hrtime(true);
+$marioGames = Jeu::where('name', 'like', '%Mario%')->get();
+$end2 = hrtime(true);
+$time2 = number_format((($end2 - $start2)/1e+6), 2, '.', ' ');
+print_r("Temps execution : " . $time2 . "\n");
+
+print_r("Q3 : \n");
+$start3 = hrtime(true);
+$marioGames = Jeu::where('name', 'like', 'Mario%')->get();
+foreach ($marioGames as $mc) {
+    foreach ($mc->characters as $character) {
+    }
+}
+$end3 = hrtime(true);
+$time3 = number_format((($end3 - $start3)/1e+6), 2, '.', ' ');
+print_r("Temps execution : " . $time3 . "\n");
+
+print_r("Q4 :\n");
+$start4 = hrtime(true);
+$jeux = Jeu::where('name', 'like', 'Mario%')->whereHas('ratings', function ($q) {
+    $q->where('name', 'like', '%3+%');
+})->get();
+foreach ($jeux as $jeu) {
+}
+$end4 = hrtime(true);
+$time4 = number_format((($end4 - $start4)/1e+6), 2, '.', ' ');
+print_r("Temps execution : " . $time4 . "\n");
