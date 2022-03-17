c53c4b06eb806e65fe2618c8e48cb0c4cf15b43d
arnoux23u <arnouxguillaume54@hotmail.fr>
2022-03-17 10:46:02 +0100

    Update Eloquent.php

--- src/db/Eloquent.php
+++ src/db/Eloquent.php
@@ -8,19 +8,22 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 class Eloquent
 {
 
+    protected static Capsule $capsule;
+
     /**
      * Static method to connect to the database
      * @param $file string config file
+     * @param $withQueryLog bool if true, queries will be logged. false as default
      */
-    public static function start(string $file)
+    public static function start(string $file, bool $withQueryLog = false)
     {
-        $capsule = new Capsule;
+        self::$capsule = new Capsule;
         if (!file_exists($file)) {
             print_r("Config file not found");
             exit();
         }
         $config = parse_ini_file($file);
-        $capsule->addConnection(array('driver' => $config['db_driver'],
+        self::$capsule->addConnection(array('driver' => $config['db_driver'],
                 'host' => $config['db_host'],
                 'database' => $config['db_name'],
                 'username' => $config['db_user'],
@@ -29,14 +32,22 @@ class Eloquent
                 'collation' => $config['db_collation'],
                 'prefix' => $config['db_prefix'])
         );
-        $capsule->setAsGlobal();
-        $capsule->bootEloquent();
+        self::$capsule->setAsGlobal();
+        self::$capsule->bootEloquent();
+        if ($withQueryLog) {
+            self::$capsule->getConnection()->enableQueryLog();
+        }
         try {
-            $capsule->getConnection()->getPdo();
+            self::$capsule->getConnection()->getPdo();
         } catch (Exception $e) {
             print_r($e->getMessage());
             print_r("\nCould not connect to the database");
             exit();
         }
+    }
+
+    public static function getQueryLog(): array
+    {
+        return self::$capsule->getConnection()->getQueryLog();
     }
 }
