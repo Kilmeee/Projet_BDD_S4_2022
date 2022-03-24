28fd2e362e6e6df67c0daaac83dc2113e083298b
xaerose <alexandre.germonville-bellet8@etu.univ-lorraine.fr>
2022-03-24 08:30:04 +0100

    base td

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+use gamepedia\db\Eloquent;
+use gamepedia\models\Jeu;
+
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+Eloquent::start(__DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'conf' . DIRECTORY_SEPARATOR . 'conf.ini');
