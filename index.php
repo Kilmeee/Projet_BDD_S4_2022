409326a1b63bea371c509c57258aa970dced352b
Kilme <keanuharrell@hotmail.fr>
2022-03-24 09:45:56 +0100

    Created 2 users

--- index.php
+++ index.php
@@ -1,8 +1,38 @@
 <?php
 
 use gamepedia\db\Eloquent;
-use gamepedia\models\Jeu;
+use gamepedia\models\{Utilisateur, Commentaire};
 
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 Eloquent::start(__DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'conf' . DIRECTORY_SEPARATOR . 'conf.ini');
+
+
+
+
+
+
+
+$u1 = new Utilisateur();
+$u2 = new Utilisateur();
+
+$c1 = new Commentaire();
+$c2 = new Commentaire();
+$c3 = new Commentaire();
+
+$u1->email = "keanuharrell@hotmail.fr";
+$u1->nom = "Harrell";
+$u1->prenom = "Keanu";
+$u1->adressedetail = "49 rue de Laxou, 54000, Nancy";
+$u1->tel = "0695098351";
+$u1->dateNaiss = date_create_from_format("Y-m-d", "2002-05-01");
+
+$u2->email = "pauladnet@hotmail.fr";
+$u2->nom = "Adnet";
+$u2->prenom = "Paul";
+$u2->adressedetail = "23 rue de l'Abbé Gridel, 54000, Nancy";
+$u2->tel = "0768249094";
+$u2->dateNaiss = date_create_from_format("Y-m-d", "2003-04-02");
+
+$u1->save();
+$u2->save();
