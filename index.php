8ea68b5c8348b8d2d8f702cd1aa668d44c59971c
Kilme <keanuharrell@hotmail.fr>
2022-03-29 15:03:28 +0200

    Q1 + Q2

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 use gamepedia\db\Eloquent;
 use Slim\{App, Container};
+use gamepedia\controllers\APIController;
 
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
@@ -14,7 +15,13 @@ $app = new App($container);
 
 #Redirection du traffic dans l'application
 $app->group('/api', function ($app) {
-    //TODO
+    $app->get("/games/{id:[0-9]+}[/]", function ($request, $response, $args) {
+        return (new APIController($this, $request, $response, $args))->getGame();
+    })->setName('createurs');
+
+    $app->get("/games[/]", function ($request, $response, $args) {
+        return (new APIController($this, $request, $response, $args))->getAllGames();
+    })->setName('createurs');
 });
 
 #Demmarage de l'application
--- src/models/Jeu.php
+++ src/models/Jeu.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 /**
  * @method static where(string $string, string $string1, string $string2)
+ * @method static limit(int $int)
+ * @method static find(int $id, array $args)
+ * @method static select(string[] $array)
  */
 class Jeu extends Model
 {
