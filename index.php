09c08fec09cb6e2fc989d241191a2fc455a43233
arnoux23u <arnouxguillaume54@hotmail.fr>
2022-03-29 14:14:47 +0200

    src files

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+use gamepedia\db\Eloquent;
+
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+Eloquent::start(__DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'conf' . DIRECTORY_SEPARATOR . 'conf.ini');
---
+++ src/models/Jeu.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace gamepedia\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @method static where(string $string, string $string1, string $string2)
+ */
+class Jeu extends Model
+{
+
+    protected $table = 'game';
+    protected $primaryKey = 'id';
+    public $incrementing = true;
+    public $timestamps = false;
+}
